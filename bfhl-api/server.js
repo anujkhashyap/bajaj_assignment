865bedc6275c2e92dd46293d1400c50645d24412
Anuj <anuj6353@gmail.com>
2025-07-29 21:11:44 +0530

    5 try

--- bfhl-api/server.js
+++ bfhl-api/server.js
@@ -12,11 +12,6 @@ app.get('/', (req, res) => {
   res.send('BFHL API is running...');
 });
 
-// Export for Vercel
-module.exports = app;
 
-// Only listen when running locally
-if (require.main === module) {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
